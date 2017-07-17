ab473cd80f733a35d4fcb1c72524385609c16f46
Emilie G <adamanska@gmail.com>
2017-07-17 15:24:12 +0200

    Working on the order and real content for chat discussion

--- public/js/main.js
+++ public/js/main.js
@@ -28,7 +28,7 @@ $.ajax({
     success: function(response) {
         chatBotGreetings = response.bot[0].greeting;
         chatBotMessage = response.bot[0].message;
-        console.log("this is letswrite " + response.bot[0].letsWrite);
+        console.log(response.bot[0].letsWrite);
         chatBotOther = response.bot[0].otherThings;
         chatBotLetsWrite = response.bot[0].letsWrite;
     },
@@ -40,24 +40,52 @@ $.ajax({
 
 $(document).ready(function() {
 
-    function a() {
+    // ========== GREETING
+    function theGreeting() {
         random(chatBotGreetings);
         $(".talk").append("<p class='chat-answers'>" + selected + "</p>");
     }
-    doItLAter(a, 2000);
+    doItLAter(theGreeting, 2000);
 
-    function b() {
+    // ======= LETS WRITE
+    function theLetsWrite() {
         random(chatBotLetsWrite);
         $(".talk").append("<p class='chat-answers'>" + selected + "</p>");
     }
-    doItLAter(b, 2000);
+    doItLAter(theLetsWrite, 2000);
 
-    function c() {
+    // ======= THE TEXT BOX
+
+    function theTextBox() {
+        $(".talk").append('<div class="st-form"> <textarea type="text" class="st-box" id="st" placeholder="Your positive self talk" name="st"></textarea></div><button class="btn btn-save" id="inspireAction">I don\'t know what to write</button><button type="submit" class="btn btn-save" id="save">Save</button>');
+    }
+
+    $(document).on("click", "#save", function() {
+        console.log($("#st").val());
+        $("#inspireAction").remove();
+        $("#st").remove();
+        $(this).remove();
+        $(".talk").append("<p class='chat-answers'> Yes I have saved the thing</p>");
+        temps = 0;
+        doItLAter(d, 2000);
+
+    }); // on click #save
+
+    // ====== OTHER THINGS
+    function d() {
+        random(chatBotOther);
+        $(".talk").append("<p class='chat-answers'>" + selected + "</p>");
+    }
+    //doItLAter(d, 2000);
+
+
+    // ======= THE BUTTONS
+    function theButtons() {
         $(".talk").append("<div class='buttons'></div>");
         $(".buttons").append("<div class='butts' id='pos'><p>YES</p></div>");
         $(".buttons").append("<div class='butts' id='neg'><p>Hell no!</p></div>");
     }
-    doItLAter(c, 2000);
+    doItLAter(theButtons, 2000);
 
     $(document).on("click", "#pos", function() {
         console.log("button clicked");
@@ -74,28 +102,7 @@ $(document).ready(function() {
         $(".buttons").remove();
         $(".talk").append("<p class='user-answers'> NOPE</p>");
         temps = 0;
-        doItLAter(e, 2000);
+        doItLAter(theTextBox, 2000);
     });
 
-    function d() {
-        random(chatBotOther);
-        $(".talk").append("<p class='chat-answers'>" + selected + "</p>");
-    }
-    //doItLAter(d, 2000);
-
-    function e() {
-        $(".talk").append('<div class="st-form"> <textarea type="text" class="st-box" id="st" placeholder="Your positive self talk" name="st"></textarea></div><button class="btn btn-save" id="inspireAction">I don\'t know what to write</button><button type="submit" class="btn btn-save" id="save">Save</button>');
-    }
-
-    $(document).on("click", "#save", function() {
-        console.log($("#st").val());
-        $("#inspireAction").remove();
-        $("#st").remove();
-        $(this).remove();
-        $(".talk").append("<p class='chat-answers'> Yes I have saved the thing</p>");
-        temps = 0;
-        doItLAter(d, 2000);
-    });
-
-
-});
+}); // document ready closing
