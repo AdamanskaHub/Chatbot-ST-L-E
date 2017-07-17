66e5e8dfa878e1373f0939ed20e8b23379cac4a0
Emilie G <adamanska@gmail.com>
2017-07-17 12:08:23 +0200

    more seed changes

--- bin/seeds.js
+++ bin/seeds.js
@@ -16,6 +16,28 @@ const bot = new chatBot({
         "Great self talk.",
         "Improving step by step.",
         "You're doing well ;)"
+    ],
+    whichTag: ["Which tag do you want to add?",
+        "Choose a tag.",
+        "Select a tag."
+    ],
+    otherThings: ["Alright, what would you like to do?",
+        "What should we do then?",
+        "Here is what we can do."
+    ],
+    inspireMe: ["Sure, here are some examples of positive self talk",
+        "I'll give you some examples of positive self talk.",
+        "Have a look at those examples of positive self talk, it might inspire you."
+    ],
+    inspireQuotes: ["I'm doing better everyday.",
+        "I can handle this.",
+        "I'll get it.",
+        "I'm amazing at that",
+        "I allow myself to fail as it allows me to improve.",
+        "I love to take action.",
+        "I like challenges and meet them head on.",
+        "I never allow the word 'no' to stop me",
+        "I live in the present moment"
     ]
 })
 
