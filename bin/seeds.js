16e57d3da1e0addc792129ee4d177b68ed52f89b
Emilie G <adamanska@gmail.com>
2017-07-17 11:43:55 +0200

    ftzughi

--- bin/seeds.js
+++ bin/seeds.js
@@ -4,8 +4,18 @@ const chatBot = require('../models/chatbot');
 mongoose.connect('mongodb://localhost/Chatbot');
 
 const bot = new chatBot({
-    greeting: ["Emilie", "Laura", "Popular Laura", "Shy Emilie"],
-    messages: ["Dislikes", "Hates", "Can't stand", "Vomits thinking about"]
+
+    greeting: ["Hello " + theUser + ", glad to see you.",
+        "Hey " + theUser + ", you're back.",
+        "I'm happy to see you " + theUser + ".",
+        theUser + " are you ready to write?"
+    ],
+
+    writeMessage: ["Dislikes",
+        "Hates",
+        "Can't stand",
+        "Vomits thinking about"
+    ]
 })
 
 chatBot.create(bot, (err, chatBot) => {
