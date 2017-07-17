a53ccef55d7a77d99d669e71dff498fa1d07da90
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-07-17 19:01:47 +0200

    commented model

--- models/content.js
+++ models/content.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const contentSchema = new Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+// const contentSchema = new Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 
-}, {
-    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
-});
+// }, {
+//     timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+// });
 
-const Content = mongoose.model("Content", contentSchema);
+// const Content = mongoose.model("Content", contentSchema);
 
-module.exports = Content;
+// module.exports = Content;
