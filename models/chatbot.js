6b160e575620139a092d248bbe6ba3aef8885a2d
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-07-17 12:34:28 +0200

    add properties to model

--- models/chatbot.js
+++ models/chatbot.js
@@ -3,7 +3,13 @@ const Schema = mongoose.Schema;
 
 const chatBotSchema = new Schema({
     greeting: Array,
-    messages: Array
+    messages: Array,
+    writeMessage: Array,
+    whichTag: Array,
+    otherThings: Array,
+    inspireMe: Array,
+    inspireQuotes: Array,
+
 
 }, {
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
